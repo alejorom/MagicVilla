28ec4a869d50cb7605ba69d0c94cead6d212d667
AlejandroRR <rromeror@indracompany.com>
2025-06-27 09:50:49 -0500

    Adicionar ModelState Validaciones

--- MagicVilla_API/Models/Dto/VillaDTO.cs
+++ MagicVilla_API/Models/Dto/VillaDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicVilla_API.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Models.Dto
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
